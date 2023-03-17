2f29bdbca26f88a2af12fcc7100ae62191fd0bc9
Wilder Fulford <fulford.wilder@gmail.com>
2023-03-16 20:16:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ class RewardFunction:
 
 if __name__ == '__main__':
     config = default_ppo_config()
-    config.model.model_path = 'EleutherAI/gpt-neo-135M'
+    config.model.model_path = 'EleutherAI/gpt-neo-125M'
     config.model.num_layers_unfrozen = 4
     config.train.seq_length = 240
     config.train.epochs = 4
